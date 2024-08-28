a0b1029418809234a5e9c58cb0aac7671e6b4c42
V1SH4N <vishandaby305@gmail.com>
2024-08-28 13:57:05 +0400

    attempt to create over effect

--- javascript.js
+++ javascript.js
@@ -9,8 +9,10 @@ for (let i = 0; i < 16; i++){
         const grid = document.createElement('div');
         grid.classList.add('grid');
         grid.style.cssText = ['border: 1px solid black; width: 40px; height: 40px;']
-        
-        // grid.addEventListener();
+        grid.addEventListener('mouseenter', (e) => {
+            console.log(e.button);
+            e.target.style.backgroundColor = 'black';
+        });
         row.appendChild(grid);
     }
     container.appendChild(row);
