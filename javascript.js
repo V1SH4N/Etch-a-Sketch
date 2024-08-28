c3271bab28515f64a8177bbae6dcdaf71565dc0f
V1SH4N <vishandaby305@gmail.com>
2024-08-28 09:48:48 +0400

    clear

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const container = document.querySelector("#container");
+container.style.cssText = ['padding: 0;margin : 100px;display: inline-block;'];
+
+for (let i = 0; i < 16; i++){
+    const row = document.createElement('div');
+    row.classList.add('row');
+    row.style.display = 'flex';
+    for (let j = 0; j < 16; j++){
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        grid.style.cssText = ['border: 1px solid black; width: 40px; height: 40px;']
+        
+        // grid.addEventListener();
+        row.appendChild(grid);
+    }
+    container.appendChild(row);
+}
+
+
