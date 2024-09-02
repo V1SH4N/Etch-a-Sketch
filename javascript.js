4e8f56dcd207b80f2bcdc76264d50360d6462f4c
V1SH4N <vishandaby305@gmail.com>
2024-09-02 08:09:42 +0400

    Create newGrid button and changed code to create grid via createGrid() function

--- javascript.js
+++ javascript.js
@@ -1,21 +1,48 @@
-const container = document.querySelector("#container");
-container.style.cssText = ['padding: 0;margin : 100px;display: inline-block;'];
 
-for (let i = 0; i < 16; i++){
-    const row = document.createElement('div');
-    row.classList.add('row');
-    row.style.display = 'flex';
-    for (let j = 0; j < 16; j++){
-        const grid = document.createElement('div');
-        grid.classList.add('grid');
-        grid.style.cssText = ['border: 1px solid black; width: 40px; height: 40px;']
-        grid.addEventListener('mouseenter', (e) => {
-            console.log(e.button);
-            e.target.style.backgroundColor = 'black';
-        });
-        row.appendChild(grid);
+const container = document.querySelector('#container');
+const newGridButton = document.querySelector('#newGridButton');
+
+container.style.cssText = ['padding: 0;margin : 100px;width: 500px; height: 500px; border: 1px solid black;display: flex;flex-direction: column;'];
+
+const DEFAULT_GRID_SIZE = 16;
+let gridSize = DEFAULT_GRID_SIZE;
+
+function createGrid(gridSize){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
     }
-    container.appendChild(row);
-}
 
+    for (let i = 0; i < gridSize; i++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        row.style.display = 'flex';
+        for (let j = 0; j < gridSize; j++){
+            const grid = document.createElement('div');
+            grid.classList.add('grid');
+            // grid.style.cssText = ['flex: 1 1 auto;aspect-ratio: 1/1;']
+            grid.style.cssText = ['border: 1px solid black;flex: 1 1 auto;aspect-ratio: 1/1;']
+            grid.addEventListener('mouseover', (e) => {
+                e.target.style.backgroundColor = 'black';
+            });
+            row.appendChild(grid);
+        }
+        container.appendChild(row);
+    }
+};
+
+newGridButton.addEventListener('click', (e) =>{
+    do{
+        gridSize = prompt("Enter new grid size");
+        
+        if (gridSize === null || gridSize === ''){;
+            gridSize = DEFAULT_GRID_SIZE;
+        }else if (gridSize > 64){
+            alert('Grid size is limited to 64 grid per side')
+        }
+    }while(gridSize > 64);
+    createGrid(gridSize);
+});
+
+
+createGrid(gridSize);
 
